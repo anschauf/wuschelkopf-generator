c1b46fc6836a6651b2cba45a7fbb93cb02050429
Andy <andreas.schaufelbuehl@gmail.com>
2022-06-27 18:45:32 +0200

    Improve Pigtail edges

--- generate_from_config.py
+++ generate_from_config.py
@@ -1,22 +1,26 @@
 import os
 from image_generator import _generate_single_image
 
-filename = 'chosen3.png'
-gender = 'male'
+gender = 'female'
+color = 'Orange'
+
+filename = f'hair_{color}_{gender}.png'
 genderpath = os.path.join('assets', gender)
+
 
 
 def main():
     output_path = os.path.join('output', 'chosen_generation', 'images')
-    configs = [
+    male_configs = [
         '00_Background\\Green.png',
         '01_Body\\SKIN_Standard_light.png',
-        '02_Clothes\\IsHairNakedShoes_Adam_Orange.png',
+        f'02_Clothes\\IsHairNakedShoes_Adam_{color}.png',
         '03_Shoes\\HasNakedShoes_Barefoot_Openlight.png',
-        '04_Hair\\HAIR_Standard_Orange.png',
+        f'04_Hair\\HAIR_Standard_{color}.png',
         '06_Mouth\\IsAlive_SootherGreen_Basic.png',
         '07_Eyes\\IsAlive_HeartsBlue_Basic.png',
         '08_Facedeko\\Tattoo.png',
+        f'09_HairExtensions\\HAIR_Pigtail_{color}.png',
         '10_Hats\\FezDefault.png',
         '11_Headdeko\\SKIN_ElvenearsAndHornsWhite_light.png',
         '12_Lefthand-Back\\SKIN_HOLDSTRAIGHT_light.png',
@@ -27,8 +31,33 @@ def main():
         '17_Righthand-Front\\SKIN_HOLDSTRAIGHT_light.png'
     ]
 
+    female_configs = [
+        '00_Background\\Green.png',
+        '01_Body\\SKIN_Standard_light.png',
+        '02_Clothes\\BikerLime.png',
+        '03_Shoes\\HasNakedShoes_Barefoot_Openlight.png',
+        f'04_Hair\\HAIR_Standard_{color}.png',
+        '05_Mouth\\IsAlive_SootherGreen_Basic.png',
+        '06_Eyes\\IsAlive_HeartsBlue_Basic.png',
+        '07_Facedeko\\Tattoo.png',
+        f'08_HairExtensions\\HAIR_Pigtail_{color}.png',
+        '09_Hats\\FezDefault.png',
+        '10_Headdeko\\SKIN_ElvenearsAndHornsWhite_light.png',
+        '11_Lefthand-Back\\SKIN_HOLDSTRAIGHT_light.png',
+        '12_Lefthand-Thing\\MULTI-SKIN_GiraffaDefault_HOLDSTRAIGHT.png',
+        '13_Lefthand-Front\\SKIN_HOLDSTRAIGHT_light.png',
+        '14_Righthand-Back\\SKIN_HOLDSTRAIGHT_light.png',
+        '15_Righthand-Thing\\MULTI-SKIN_PennonMint_HOLDSTRAIGHT.png',
+        '16_Righthand-Front\\SKIN_HOLDSTRAIGHT_light.png'
+    ]
+
+
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+
+    configs = male_configs
+    if gender == 'female':
+        configs = female_configs
 
     _generate_single_image(configs, genderpath, os.path.join(output_path, filename))
     print("Generated")
