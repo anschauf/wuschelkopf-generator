4eb5774ffbff1b729caf5327ec624a3fbd19952e
Andy <andreas.schaufelbuehl@gmail.com>
2022-06-28 00:21:24 +0200

    Adapt chinballs

--- generate_from_config.py
+++ generate_from_config.py
@@ -3,8 +3,9 @@ from image_generator import _generate_single_image
 
 gender = 'male'
 color = 'ShiningOrange'
+skin = 'Ghost'
 
-filename = f'hair_{color}_Chinballs_{gender}.png'
+filename = f'hair_{color}_Chinballs_{skin}_{gender}.png'
 genderpath = os.path.join('assets', gender)
 
 
@@ -13,21 +14,21 @@ def main():
     output_path = os.path.join('output', 'chosen_generation', 'images')
     male_configs = [
         '00_Background\\Green.png',
-        '01_Body\\SKIN_Standard_light.png',
+        f'01_Body\\SKIN_Standard_{skin}.png',
         f'02_Clothes\\IsHairNakedShoes_Adam_{color}.png',
-        '03_Shoes\\HasNakedShoes_Barefoot_Openlight.png',
+        f'03_Shoes\\HasNakedShoes_Barefoot_Open{skin}.png',
         f'04_Hair\\HAIR_Standard_{color}.png',
-        f'05_Chindeko\\SKIN_Chinballs_light.png',
+        f'05_Chindeko\\SKIN_Chinballs_{skin}.png',
         '06_Mouth\\IsAlive_SootherGreen_Basic.png',
         '07_Eyes\\IsAlive_HeartsBlue_Basic.png',
         '10_Hats\\FezDefault.png',
         f'11_Headdeko\\HAIR_Sheep_{color}.png',
-        '12_Lefthand-Back\\SKIN_HOLDSTRAIGHT_light.png',
+        f'12_Lefthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
         '13_Lefthand-Thing\\MULTI-SKIN_GiraffaDefault_HOLDSTRAIGHT.png',
-        '14_Lefthand-Front\\SKIN_HOLDSTRAIGHT_light.png',
-        '15_Righthand-Back\\SKIN_HOLDSTRAIGHT_light.png',
+        f'14_Lefthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png',
+        f'15_Righthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
         '16_Righthand-Thing\\MULTI-SKIN_PennonMint_HOLDSTRAIGHT.png',
-        '17_Righthand-Front\\SKIN_HOLDSTRAIGHT_light.png'
+        f'17_Righthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png'
     ]
 
     female_configs = [
