feb842e14f96e031d338c13d354827aef0f51944
Andy <andreas.schaufelbuehl@gmail.com>
2022-06-28 23:33:09 +0200

    Improve hands again but better

--- generate_from_config.py
+++ generate_from_config.py
@@ -2,10 +2,11 @@ import os
 from image_generator import _generate_single_image
 
 gender = 'female'
-color = 'Green'
-skin = 'Ghost'
+color = 'ShiningPurple'
+skin = 'choco'
 
-filename = f'hair_{color}_Chinballs_{skin}_{gender}.png'
+# filename = f'hair_{color}_Chinballs_{skin}_{gender}.png'
+filename = f'Claudia_choco.png'
 genderpath = os.path.join('assets', gender)
 
 
@@ -32,22 +33,21 @@ def main():
     ]
 
     female_configs = [
-        '00_Background\\Green.png',
+        '00_Background\\Lightblue.png',
         f'01_Body\\SKIN_Standard_{skin}.png',
-        '02_Clothes\\BikerLime.png',
-        f'03_Shoes\\HasNakedShoes_Barefoot_Open{skin}.png',
+        '02_Clothes\\JediDefault.png',
+        f'03_Shoes\\HasNakedShoes_Barefoot_Closed{skin}.png',
         f'04_Hair\\HAIR_Standard_{color}.png',
-        '05_Mouth\\IsAlive_SootherGreen_Basic.png',
-        '06_Eyes\\IsAlive_HeartsBlue_Basic.png',
+        '05_Mouth\\IsAlive_Fangs_Basic.png',
+        '06_Eyes\\IsAlive_ShiningPurple_Basic.png',
         '07_Facedeko\\Tattoo.png',
-        f'08_HairExtensions\\HAIR_Pigtail_{color}.png',
-        '09_Hats\\FezDefault.png',
-        f'10_Headdeko\\HAIR_Sheep_{color}.png',
+        # '09_Hats\\WizardhatRed.png',
+        f'10_Headdeko\\SKIN_ElvenearsAndHornsWhite_{skin}.png',
         f'11_Lefthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
-        '12_Lefthand-Thing\\MULTI-SKIN_GiraffaDefault_HOLDSTRAIGHT.png',
+        '12_Lefthand-Thing\\MULTI-SKIN_TurtleAlbino_HOLDSTRAIGHT.png',
         f'13_Lefthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png',
         f'14_Righthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
-        '15_Righthand-Thing\\MULTI-SKIN_PennonMint_HOLDSTRAIGHT.png',
+        '15_Righthand-Thing\\MULTI-SKIN_MagicWandWhite_HOLDSTRAIGHT.png',
         f'16_Righthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png'
     ]
 
