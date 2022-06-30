607073ab5251dbd1811190835fbce918c57fe8a0
Andy <andreas.schaufelbuehl@gmail.com>
2022-06-30 15:24:56 +0200

    Adapt elefant position

--- generate_from_config.py
+++ generate_from_config.py
@@ -3,7 +3,7 @@ from image_generator import _generate_single_image
 
 gender = 'female'
 color = 'ShiningPurple'
-skin = 'choco'
+skin = 'Skeleton'
 
 # filename = f'hair_{color}_Chinballs_{skin}_{gender}.png'
 filename = f'Claudia_choco.png'
@@ -38,13 +38,13 @@ def main():
         '02_Clothes\\JediDefault.png',
         f'03_Shoes\\HasNakedShoes_Barefoot_Closed{skin}.png',
         f'04_Hair\\HAIR_Standard_{color}.png',
-        '05_Mouth\\IsAlive_Fangs_Basic.png',
-        '06_Eyes\\IsAlive_ShiningPurple_Basic.png',
+        # '05_Mouth\\IsAlive_Fangs_Basic.png',
+        # '06_Eyes\\IsAlive_ShiningPurple_Basic.png',
         '07_Facedeko\\Tattoo.png',
         # '09_Hats\\WizardhatRed.png',
         f'10_Headdeko\\SKIN_ElvenearsAndHornsWhite_{skin}.png',
         f'11_Lefthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
-        '12_Lefthand-Thing\\MULTI-SKIN_TurtleAlbino_HOLDSTRAIGHT.png',
+        '12_Lefthand-Thing\\MULTI-SKIN_ElephantBrown_HOLDSTRAIGHT.png',
         f'13_Lefthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png',
         f'14_Righthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
         '15_Righthand-Thing\\MULTI-SKIN_MagicWandWhite_HOLDSTRAIGHT.png',
