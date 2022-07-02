292dd3efab613878f4ed864a704644f360959912
Andy <andreas.schaufelbuehl@gmail.com>
2022-07-02 12:18:56 +0200

    Adapt the walking stick to not cross the shoes

--- generate_from_config.py
+++ generate_from_config.py
@@ -1,12 +1,12 @@
 import os
 from image_generator import _generate_single_image
 
-gender = 'female'
+gender = 'male'
 color = 'Blue'
-skin = 'Skeleton'
+skin = 'choco'
 
 # filename = f'hair_{color}_Chinballs_{skin}_{gender}.png'
-filename = f'rubinleft.png'
+filename = f'male_stick.png'
 genderpath = os.path.join('assets', gender)
 
 
@@ -25,7 +25,7 @@ def main():
         '10_Hats\\FezDefault.png',
         f'11_Headdeko\\HAIR_Sheep_{color}.png',
         f'12_Lefthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
-        '13_Lefthand-Thing\\MULTI-SKIN_GiraffaDefault_HOLDSTRAIGHT.png',
+        '13_Lefthand-Thing\\MULTI-SKIN_WalkingstickWhite_HOLDSTRAIGHT.png',
         f'14_Lefthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png',
         f'15_Righthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
         '16_Righthand-Thing\\MULTI-SKIN_PennonMint_HOLDSTRAIGHT.png',
@@ -38,15 +38,15 @@ def main():
         '02_Clothes\\IsNakedShoes_CocktaildressSalmon.png',
         f'03_Shoes\\ConverseBlack.png',
         f'04_Hair\\HAIR_Standard_{color}.png',
-        # '05_Mouth\\IsAlive_Fangs_Basic.png',
-        'Skeleton_Mouth\\BlingGold.png',
-        # '06_Eyes\\IsAlive_ShiningPurple_Basic.png',
-        'Skeleton_Eyes\\Rubinleft.png',
+        '05_Mouth\\IsAlive_Fangs_Basic.png',
+        # 'Skeleton_Mouth\\BlingGold.png',
+        '06_Eyes\\IsAlive_ShiningPurple_Basic.png',
+        # 'Skeleton_Eyes\\Rubinleft.png',
         '07_Facedeko\\Tattoo.png',
         # '09_Hats\\WizardhatRed.png',
         f'10_Headdeko\\SKIN_ElvenearsAndHornsWhite_{skin}.png',
         f'11_Lefthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
-        '12_Lefthand-Thing\\MULTI-SKIN_ElephantBrown_HOLDSTRAIGHT.png',
+        '12_Lefthand-Thing\\MULTI-SKIN_WalkingstickWhite_HOLDSTRAIGHT.png',
         f'13_Lefthand-Front\\SKIN_HOLDSTRAIGHT_{skin}.png',
         f'14_Righthand-Back\\SKIN_HOLDSTRAIGHT_{skin}.png',
         '15_Righthand-Thing\\MULTI-SKIN_MagicWandWhite_HOLDSTRAIGHT.png',
